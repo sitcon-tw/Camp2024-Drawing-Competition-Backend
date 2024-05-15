83dd98388cb8debaafe4e71e111d22f3e8579eb8
fan9704 <cxz123499@gmail.com>
2024-05-15 12:09:04 +0800

    (views): 取消 post 路由
    取消掛載路由

--- api/views/round.py
+++ api/views/round.py
@@ -1,22 +1,16 @@
-import logging
-
 from api.models import Round
-from drf_yasg import openapi
-from drf_yasg.utils import swagger_auto_schema
 from rest_framework import generics
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from api.serializers.round import (
     RoundGeneralSerializer,
 )
 
-class RoundListCreateAPIView(generics.ListCreateAPIView):
+
+class RoundListCreateAPIView(generics.ListAPIView):  # 列出所有回合
     queryset = Round.objects.all()
     serializer_class = RoundGeneralSerializer
 
-    
+
 class RoundAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Round.objects.all()
     serializer_class = RoundGeneralSerializer
