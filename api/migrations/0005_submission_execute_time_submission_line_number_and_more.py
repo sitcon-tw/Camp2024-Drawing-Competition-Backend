892ec9741d167674ace90bfe1c808694aa0b3f80
aa35037123 <wesleyshi0321@gmail.com>
2024-05-19 17:15:37 +0800

    update: submission attribute

--- api/migrations/0005_submission_execute_time_submission_line_number_and_more.py
+++ api/migrations/0005_submission_execute_time_submission_line_number_and_more.py
@@ -19,7 +19,7 @@ class Migration(migrations.Migration):
         ),
         migrations.AddField(
             model_name='submission',
-            name='word_count',
+            name='line_number',
             field=models.IntegerField(default=0, verbose_name='行數'),
         ),
         migrations.AddField(
