77d9ef958d225712f476d3ea6b6f100bf42a96e8
fan9704 <cxz123499@gmail.com>
2024-05-19 23:49:40 +0800

    (round): time > datetime

--- api/model/round.py
+++ api/model/round.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Round(models.Model):
-    start_time = models.TimeField(null=False)
-    end_time = models.TimeField(null=False)
+    start_time = models.DateTimeField(null=False)
+    end_time = models.DateTimeField(null=False)
     is_valid = models.BooleanField(default=True)
 
     def __str__(self):
