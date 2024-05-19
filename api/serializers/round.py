8255a3eed5f8e966ca89c1e4a544e6052f02742f
fan9704 <cxz123499@gmail.com>
2024-05-19 15:51:17 +0800

    ✨ feat(round): list with challenge
    add challenge_list field
    
    https://github.com/fan9704/Drawing-Competition-Backend/issues/7

--- api/serializers/round.py
+++ api/serializers/round.py
@@ -1,8 +1,20 @@
 from api.models import Round
 from rest_framework import serializers
+from api.serializers.challenge import ChallengeGeneralSerializer
 
 
 class RoundGeneralSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Round
+        fields = "__all__"
+        read_only_fields = ("id",)
+
+
+class RoundChallengeSerializer(serializers.ModelSerializer):
+    challenge_list = ChallengeGeneralSerializer(
+        source="challenge_set", many=True, read_only=True
+    )
+
     class Meta:
         model = Round
         fields = "__all__"
