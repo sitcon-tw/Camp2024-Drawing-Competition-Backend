98429fe82f2966cc5bd5439d1b86218d6c8c1924
fan9704 <cxz123499@gmail.com>
2024-05-19 20:06:07 +0800

    ✨ feat(team): team auth with jwt
    after login will recieve JWT Token

--- api/views/team.py
+++ api/views/team.py
@@ -6,6 +6,7 @@ from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import AccessToken
 
 from api.serializers.team import (
     TeamGeneralSerializer,
@@ -71,3 +72,33 @@ class TeamAuthAPIView(APIView):
             )
         else:
             return Response({"status": False, "team": None}, status=status.HTTP_200_OK)
+
+
+class TeamAuthAPIView(APIView):
+
+    @swagger_auto_schema(
+        operation_summary="Auth team",
+        operation_description="Auth team with token",
+        tags=["team"],
+        request_body=TeamAuthSerializer,
+    )
+    def post(self, request):
+        data = request.data
+        serializer = TeamAuthSerializer(data=data)
+        team = Team.objects.filter(name=data["name"], token=data["token"]).first()
+        if team:
+            # Generate a new access token
+            access_token = AccessToken.for_user(team)
+            return Response(
+                {
+                    "status": True,
+                    "team": TeamGeneralSerializer(team).data,
+                    "access_token": str(access_token),
+                },
+                status=status.HTTP_200_OK,
+            )
+        else:
+            return Response(
+                {"status": False, "team": None, "access_token": None},
+                status=status.HTTP_200_OK,
+            )
