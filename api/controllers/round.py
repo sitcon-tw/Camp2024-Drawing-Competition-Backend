80388983ad2817a971b34404aeaceb7b17d35bef
fan9704 <cxz123499@gmail.com>
2024-05-27 11:02:13 +0800

    ✨ feat(round): implement end round
    End Round  -> 204
    Current Time has Round -> 200
    Not Valid Time Round -> 404
    
    https://github.com/fan9704/Drawing-Competition-Backend/issues/29

--- api/controllers/round.py
+++ api/controllers/round.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from api.views.round import RoundAPIView, RoundListCreateAPIView
+from api.views.round import RoundAPIView, RoundListAPIView
 
 urlpatterns = [
     # Round Routes
-    path("", RoundListCreateAPIView.as_view(), name="round"),
+    # 列出符合當前時間的回合
+    path("", RoundListAPIView.as_view(), name="round"),
+    # 取得特定 ID 的回合資訊
     path("<int:pk>/", RoundAPIView.as_view(), name="round-detail"),
 ]
