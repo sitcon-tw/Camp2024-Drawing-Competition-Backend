8ce5d5c4dc751ed21c03738bbfca9dc8780f94f5
aa35037123 <wesleyshi0321@gmail.com>
2024-06-02 23:04:14 +0800

    fix: submission fitness

--- judge.py
+++ judge.py
@@ -2,10 +2,8 @@ import subprocess
 import os
 from PIL import Image
 import time
-import platform
-import numpy as np
 from skimage.metrics import structural_similarity as ssim
-
+import numpy as np
 
 def get_word_count(file_path):
     with open(file_path, 'r') as file:
@@ -15,7 +13,7 @@ def get_word_count(file_path):
 
 def linear_normalize(value, min_value, max_value):
     return (value - min_value) / (max_value - min_value)
-
+    
 def convert_ps_to_png(ps_file, png_file):
     # Ensure the output directory exists
     # Open the PostScript file and convert it to a PNG file
@@ -63,10 +61,10 @@ def judge_logic(image_url, result_path, word_count, execution_time):
     total_score = similarity_score + word_count_score + execution_time_score
     print(f"Similarity score: {similarity_score}, Word Count score: {word_count_score}, Execution Time score: {execution_time_score}\n\n")
     # Return the similarity score
-    return total_score, similarity
+    return total_score, similarity*100
 
 def run_code(code_path, image_url, result_path):
-
+    
     result_dir = os.path.dirname(result_path)
     ps_file = f"{result_dir}/temp.ps"
     # png_file = f"{result_dir}/{output_filename}.png"
@@ -78,18 +76,14 @@ def run_code(code_path, image_url, result_path):
     # Use check to raise an exception if the script fails
     start_time = time.time()
     try:
-        # Detect Run Time OS
-        if platform.system() == "Windows":
-            subprocess.run(["python", code_path, ps_file], check=True)
-        else:
-            subprocess.run(["python3", code_path, ps_file], check=True)
+        subprocess.run(['python3', code_path, ps_file], check=True)
     except subprocess.CalledProcessError as e:
         # Handle errors in the subprocess
-        print(f"Subprocess CalledProcessError: {e}")
+        print(f"Error: {e}")
         return None
     end_time = time.time()
     execution_time = end_time - start_time
-
+    
     # check if the PostScript file was created
     if not os.path.exists(ps_file):
         return 0, 0, word_count, execution_time
